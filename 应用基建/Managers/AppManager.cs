2bf3e3eeb54cd59b19b69e6478f43687bfbf14f9
shikii <123>
2018-07-08 21:08:16 +0800

    像Android那样自动管理弹出对话框。

--- 应用基建/Managers/AppManager.cs
+++ 应用基建/Managers/AppManager.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Text;
 using System.Windows.Forms;
 using Common;
+using dotNetLab.Forms;
 
 namespace dotNetLab
 {
@@ -36,11 +37,26 @@ namespace dotNetLab
             public static Form ShowPage(Type typeForm)
             {
                 Form frm = System.Activator.CreateInstance(typeForm) as Form;
+                frm.StartPosition = FormStartPosition.CenterScreen;
                 frm.Show();
                 frm.FormClosed += (s, e) =>
                 {
                     frm.Dispose();
                 };
+                return frm;
+            }
+            public static Form ShowFixedPage(Type typeForm)
+            {
+                Form frm = ShowPage(typeForm) ;
+                if(frm is ModernWnd)
+                {
+                    ModernWnd wnd = frm as ModernWnd;
+                    wnd.EnableDialog = true;
+                }
+                else
+                {
+                    frm.FormBorderStyle = FormBorderStyle.FixedDialog;
+                }
                 return frm;
             }
             public void InitSubForm<T>(out T frm)
