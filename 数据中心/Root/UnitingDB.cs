70eebc075f242f4c8d433822114245495254b5f8
shikii <shikii@outlook.com>
2018-12-16 23:28:35 +0800

    temp

---
+++ 数据中心/Root/UnitingDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotNetLab.Data 
+{
+    public abstract class   DB 
+    {
+        protected String DBName;
+        protected int nPort;
+        protected String strIP;
+        public String Database
+        {
+            get { return DBName; }
+            set { DBName = value; }
+        }
+    }
+}
